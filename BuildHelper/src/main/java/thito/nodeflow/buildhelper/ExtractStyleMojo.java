ada7666db4f8415866aaebbf99c896c421eb788b
sunarya-thito <septogeddon@gmail.com>
2021-09-15 20:55:40 +0700

    Added build-helper plugin

--- BuildHelper/src/main/java/thito/nodeflow/buildhelper/ExtractStyleMojo.java
+++ BuildHelper/src/main/java/thito/nodeflow/buildhelper/ExtractStyleMojo.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.concurrent.atomic.*;
 import java.util.stream.*;
 
-@Mojo(name = "extract-style", defaultPhase = LifecyclePhase.PACKAGE)
+@Mojo(name = "extract-style")
 public class ExtractStyleMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
